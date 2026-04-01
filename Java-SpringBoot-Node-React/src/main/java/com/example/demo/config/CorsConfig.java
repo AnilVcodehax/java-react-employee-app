1401fae4d54b2edd1ae8a47a8a163d3744f346d7
AnilVcodehax <anil.karlapudi@infovision.com>
2026-04-01 20:44:13 +0530

    Update CorsConfig.java
    added imports

--- Java-SpringBoot-Node-React/src/main/java/com/example/demo/config/CorsConfig.java
+++ Java-SpringBoot-Node-React/src/main/java/com/example/demo/config/CorsConfig.java
@@ -2,8 +2,11 @@ package com.example.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.cors.*;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 import java.util.List;
@@ -37,9 +40,11 @@ public class CorsConfig {
             "https://anil-employee-front-end-onrender.com"
         ));
         config.setAllowedHeaders(List.of("*"));
+       
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+       
         source.registerCorsConfiguration("/**", config);
 
         return new CorsFilter(source);
