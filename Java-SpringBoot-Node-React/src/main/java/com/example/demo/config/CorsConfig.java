7f9238fd705150f4d26d85e5734f1564ef692077
AnilVcodehax <anil.karlapudi@infovision.com>
2026-04-01 18:56:31 +0530

    Fix Cors for backend CorsConfig.java

--- Java-SpringBoot-Node-React/src/main/java/com/example/demo/config/CorsConfig.java
+++ Java-SpringBoot-Node-React/src/main/java/com/example/demo/config/CorsConfig.java
@@ -3,11 +3,16 @@ package com.example.demo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.cors.*;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List
 
 @Configuration
 public class CorsConfig {
 
-    @Bean
+   /* @Bean
+    //This is for local host
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
@@ -18,5 +23,26 @@ public class CorsConfig {
                         .allowedHeaders("*");
             }
         };
+    }*/
+
+    // For deploy on render.com
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+
+        config.setAllowedOrigins(List.of(
+            "https://anil-employee-front-end-onrender.com"
+        ));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
+        UrlBaseCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
     }
+    
 }
